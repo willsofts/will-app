7f3e3fb9f4e07afde064f4a32fdbdf90c03aa882
tassun <32536248+tassun@users.noreply.github.com>
2024-09-13 13:38:28 +0700

    fixed render paging

--- app/Paging.js
+++ app/Paging.js
@@ -53,8 +53,8 @@ class Paging {
         }
         let fsPreviousPage = fsCounter * fsLimit;
         if (fsLimit > 0 && (fsPages > fsLimit)) {
-            let first = { page: 1, text: "|&lt;", css: "" };
-            let previous = { page: fsPreviousPage, text: "&lt;&lt;", css: "" };
+            let first = { page: 1, text: "|<", css: "" };
+            let previous = { page: fsPreviousPage, text: "<<", css: "" };
             results.push(first);
             results.push(previous);
         }
@@ -70,7 +70,7 @@ class Paging {
                     if (fsPageNumber > fsTotalPage) {
                         fsPageNumber = fsTotalPage;
                     }
-                    let next = { page: fsPageNumber, text: "&gt;&gt;", css: "" };
+                    let next = { page: fsPageNumber, text: ">>", css: "" };
                     results.push(next);
                     break;
                 }
@@ -87,7 +87,7 @@ class Paging {
             for (let i = 0; i < fsRows; i += fsChapters) {
                 fsPageNumber++;
             }
-            let last = { page: fsPageNumber, text: "&gt;|", css: "" };
+            let last = { page: fsPageNumber, text: ">|", css: "" };
             results.push(last);
         }
         return results;
